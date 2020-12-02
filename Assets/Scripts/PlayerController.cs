8bba9b9648bd96afb91dd3bcce8cfa670c5923ae
cvr-software <cvrgaming@gmail.com>
2020-12-02 14:30:13 -0800

    Lesson 2.3 Complete

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject projectilePrefab;
+    
+    public float horizontalInput;
+
+    public float speed = 20.0f;
+    public float xRange = 10.0f;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        //Move player left/right based on horizontal input
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalInput * Time.deltaTime * speed);
+
+        //Keep player in bounds based on xRange variable
+        if (transform.position.x > xRange) transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        if (transform.position.x < -xRange) transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+
+        //If space is pressed, instantiate new projectile
+        if (Input.GetKeyDown(KeyCode.Space)) Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+    }
+}
---
+++ Assets/Scripts/SpawnManager.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnManager : MonoBehaviour
+{
+    public GameObject[] animalPrefabs;
+
+    private float spawnRangeX = 20.0f;
+    private float spawnPosZ = 20.0f;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.S))
+        {
+            //Randomly generate animal index & spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+            int animalIndex = Random.Range(0, animalPrefabs.Length);
+            
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
+        }
+    }
+}
