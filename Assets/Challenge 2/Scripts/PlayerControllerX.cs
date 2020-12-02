6b03938d03766492641f6b5e66eccccebd4405a0
cvr-software <cvrgaming@gmail.com>
2020-12-02 15:03:21 -0800

    Challenge 2 Complete

---
+++ Assets/Challenge 2/Scripts/PlayerControllerX.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControllerX : MonoBehaviour
+{
+    public GameObject dogPrefab;
+
+    public float dogSpawnCooldown = 1.0f;
+
+    public float dogSpawnTimeCount;
+
+    // Update is called once per frame
+    void Update()
+    {
+        // On spacebar press, send dog
+        if (Input.GetKeyDown(KeyCode.Space) && dogSpawnTimeCount > dogSpawnCooldown * 60 * (1 - Time.deltaTime))
+        {
+            Instantiate(dogPrefab, transform.position, dogPrefab.transform.rotation);
+            dogSpawnTimeCount = 0;
+        }
+
+        dogSpawnTimeCount++;
+    }
+}
